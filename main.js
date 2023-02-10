1b0d90b34ff5a313bd0bfb7635fcd10dffc8a73b
Shaina Couch <shainacouch21@gmail.com>
2023-02-10 10:51:50 -0600

    old, simple, javascript proj

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay"]
+const toFireInKiln = []
+
+for (const makinMugs of clay) {
+    const mug = `${makinMugs} coffee mug`
+    toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
